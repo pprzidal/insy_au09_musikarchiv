6c1530876e0e72a8dbaa775e6310342bd03531cf
pprzidal <pprzidal@student.tgm.ac.at>
2020-05-02 19:59:16 +0200

    topf 2 (Adele) inserted album "21"

---
+++ topf2.sql
@@ -0,0 +1,17 @@
+USE musikarchiv2020;
+
+-- Nr.: 3 which would correspond to Adele
+
+UPDATE interpret SET wikipedia = "https://de.wikipedia.org/wiki/Adele_(S%C3%A4ngerin)" WHERE iname = "Adele";
+
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Rolling In The Deep");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Rumour Has It");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Turning Tables");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Don't You Remember");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Set Fire To The Rain");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "He Won't Go");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Take It All");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "I'll Be Waiting");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "One And Only");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Lovesong");
+INSERT INTO song (iname, stitel) VALUES ("Adele", "Someone Like You");
